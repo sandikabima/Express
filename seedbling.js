9bd651bc592bec1f512b70ed0edbf7c9cee4362b
Bima sandika <bimasandika@gmail.com>
2024-04-12 19:12:27 -0700

    Express

--- seedbling.js
+++ seedbling.js
@@ -1,4 +1,5 @@
 const pool = require('./conn')
+const fs = require('fs')
 
 const createCostumers = `CREATE TABLE IF NOT EXISTS "Costumers" (
     "Id" serial primary key,
@@ -11,11 +12,29 @@ const createCostumers = `CREATE TABLE IF NOT EXISTS "Costumers" (
 );
 `
 
-const insertCostumer = ``
+let insertCostumer = `INSERT INTO "Costumers" ("Name","Email","Address","City","PostalCode","Country") VALUES`
 
-pool.query(createCostumers, (err , res) => {
+fs.readFile('./costumers.json', 'utf8' , (err, data)=>{
     if (err) throw err
-    console.log('Success create table costumers')
-    
-    pool.end()
+    data = JSON.parse(data)
+    data.map((e, i)=>{
+        insertCostumer += `('${e.CustomerName}','${e.Email}','${e.Address}','${e.City}','${e.PostalCode}','${e.Country}')`
+        if (i === data.length - 1) insertCostumer += ';'
+        else {
+            insertCostumer += ','
+        }
+    })
+
+    pool.query(createCostumers, (err , res) => {
+        if (err) throw err
+        console.log('Success create table costumers')
+        pool.query(insertCostumer , (err, res)=> {
+            if (err) throw err
+            console.log('berhasil')
+            pool.end()
+        })
+    })
 })
+
+
+
